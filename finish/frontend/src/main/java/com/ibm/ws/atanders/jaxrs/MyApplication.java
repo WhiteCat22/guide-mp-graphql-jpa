2db764b17daa5f659b623aeb7f0a9de15e569fbd
Adam Anderson <atanderson9383@gmail.com>
2021-10-12 09:23:10 -0500

    update application to use the GraphiQL interface instead of the JAX-RS frontend

--- finish/frontend/src/main/java/com/ibm/ws/atanders/jaxrs/MyApplication.java
+++
@@ -1,7 +0,0 @@
-package com.ibm.ws.atanders.jaxrs;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/Dealership")
-public class MyApplication extends Application {}
